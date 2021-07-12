4918f9071a8f46f8c7060c8db6f041f46753b935
ShubhamS318 <72723977+ShubhamS318@users.noreply.github.com>
2021-07-12 04:01:19 -0700

    Add files via upload

---
+++ THA 11/index.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "The ‘Big Bang Theory’ is related to which of the following?",
+    answer: "Origin of Universe",
+    options: [
+      "Continental Drift",
+      "Origin of Universe",
+      "Origin of Himalayas",
+      "Eruption of Volcanoes"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The Milky Way is classified as_________.",
+    answer: "Spiral Galaxy",
+    options: [
+      "Electrical Galaxy",
+      "Spiral Galaxy",
+      "Computer Style Sheet",
+      "Round Galaxy"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does the colour of star indicates?",
+    answer: "Temperature",
+    options: [
+      "Distance from the Sun",
+      " Distance from the earth",
+      "Lighting or glow",
+      "Temperature"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the time taken by the Sun to revolve around the centre of our galaxy?",
+    answer: "25 Crore years",
+    options: [
+      "2.5 Crore years",
+      "10 Crore years",
+      "25 Crore years",
+      "50 Crore years"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following concept related to the limit beyond stars suffer internal collapse?",
+    answer: "Chandrasekhar limit",
+    options: [
+      "Eddington limit",
+      " Hoyle limit",
+      "Chandrasekhar limit",
+      "Fowler limit"
+    ]
+  },
+];
+  
